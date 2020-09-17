29f591759ea211b25cb3ac4e22dc59888673a16d
jinwook.im <jin3670@gmail.com>
2020-09-17 11:40:13 +0900

    spring-boot autoconfigurationTest

---
+++ src/main/java/me/jinwook/Application.java
@@ -0,0 +1,15 @@
+package me.jinwook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.WebApplicationType;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication application = new SpringApplication(Application.class);
+        application.setWebApplicationType(WebApplicationType.NONE);
+        application.run(args);
+    }
+}
