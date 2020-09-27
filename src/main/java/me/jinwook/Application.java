d480ac64692a718b3bd1b4ed252172abf52c733f
jinwook.im <jin3670@gmail.com>
2020-09-27 19:14:01 +0900

    add Banner

--- src/main/java/me/jinwook/Application.java
+++ src/main/java/me/jinwook/Application.java
@@ -1,5 +1,6 @@
 package me.jinwook;
 
+import org.springframework.boot.Banner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -10,6 +11,12 @@ public class Application {
     public static void main(String[] args) {
         SpringApplication application = new SpringApplication(Application.class);
         application.setWebApplicationType(WebApplicationType.NONE);
+        application.setBanner((environment, sourceClass, out) -> {
+            out.println("================");
+            out.println("aaaaaaa----");
+            out.println("================");
+        });
+        //application.setBannerMode(Banner.Mode.OFF);
         application.run(args);
     }
 }
